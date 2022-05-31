1ee9eaf6218fc54ceab42dc8bf3718699ef50008
032790341619 <igorlimaesilva94@gmail.com>
2022-05-31 13:32:57 -0300

    Mensagens mais amigaveis no prompt

--- SUETools.py
+++ SUETools.py
@@ -1,5 +1,5 @@
 import subprocess
-import os
+import time
 
 numero_usb = 0
 
@@ -18,27 +18,45 @@ def quantidade_usb():
 def detectar_usb_e_formatar():
     global array
     global numero_usb
+
     array.clear()
 
+    subprocess.run("cls", shell=True)
+
+    print("Insira as mídias, " + str(numero_usb) + " no total.")
+
     while True:
-        result = str(subprocess.check_output("wmic logicaldisk where drivetype=2 get DeviceID", text=True)).replace(
+        result = str(
+            subprocess.check_output("wmic logicaldisk where drivetype=2 get DeviceID", text=True,
+                                    stderr=subprocess.DEVNULL)).replace(
             "\r", "").replace("\n", "").replace("DeviceID  ", "").replace(":        ", "|")
         if str(result) != "":
             array = result.split("|")
             array.remove("")
-            print(str(array))
+            time.sleep(1)
+            subprocess.run("cls", shell=True)
+            print("Insira as mídias, " + str(numero_usb) + " no total.\n")
+            print("Número de mídias inseridas: " + str(len(array)) + "\n")
+            print(str(array) + "\n")
             if len(array) != int(numero_usb):
                 array.clear()
             else:
                 break
 
     for usb in array:
-        os.system("format /q /x /y " + usb + ":")
+        result = subprocess.run("format /q /x /y " + usb + ":", shell=True, stdout=subprocess.DEVNULL,
+                                stderr=subprocess.STDOUT)
+        if str(result.returncode) == "0":
+            print("Unidade " + usb + " Formatada com sucesso.")
+        else:
+            print("Unidade " + usb + " Formatação falhou.")
+
+    print("\nRemova todas as Mídias")
 
     while True:
-        result = str(subprocess.check_output("wmic logicaldisk where drivetype=2 get DeviceID", text=True)).replace(
+        result = str(subprocess.check_output("wmic logicaldisk where drivetype=2 get DeviceID", text=True,
+                                             stderr=subprocess.DEVNULL)).replace(
             "\r", "").replace("\n", "").replace("DeviceID  ", "").replace(":        ", "")
-        print("Remova todas as USBs")
         if result == "":
             break
 
