7095c7f3f8b0ac14d483e0c5fff63263291287b8
ILS <99141005+ils94@users.noreply.github.com>
2022-07-14 16:52:11 -0300

    Delete compilar.bat

--- compilar.bat
+++
@@ -1,5 +0,0 @@
-C:\Users\032790341619\Documents\Pythons\Python-SUETOOLS\python.exe C:\Users\032790341619\Documents\Pythons\Python-SUETOOLS\Scripts\pyinstaller.exe SUETools.py --icon=icones\exeicon.ico --onedir
-
-xcopy audios dist\SUETools\audios\
-
-pause
